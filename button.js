8d425137f601f9b1992f5a72169d4c3a22130d00
zhouyoujun <a390687273@qq.com>
2023-04-17 00:35:51 +0800

    加入parcel，将button组件改为单文件

--- button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('g-button', {
-    template: `
-    <div> <button class= "g-button"> hi </button></div> 
-    `
-})
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Button from '../src/button'
+Vue.component('g-button', Button)
+
+new Vue({
+    el: '#app',
+})
