11e92029737f0d1a6f3117e605eb1ac3193df84f
zhouyoujun <a390687273@qq.com>
2023-04-18 15:00:37 +0800

    用 validator（）验证iconPosition为left 或者right

--- src/button.vue
+++ src/button.vue
@@ -12,7 +12,22 @@
 
 <script>
 export default {
-    props: ['icon', 'iconPosition']
+    // props: ['icon', 'iconPosition']
+    props: {
+        icon: {},
+        iconPosition: {
+            type: String,
+            default: 'left',
+            validator(value) {
+                // if (value !== 'left' && value !== 'right') {
+                //     return false
+                // } else {
+                //     return true
+                // }
+                return value === "left" || value === 'right'
+            }
+        }
+    }
 }
 
 </script>
